2f529e07063c1a584ed2efe6be49a0dbbd8218c9
AzureAD\RishabhMehta <rishabh@estater.com>
2024-04-20 17:07:24 +0530

    push test

---
+++ codingJava/src/day5CarryFwdSubArray/Return2DSubArray.java
@@ -0,0 +1,13 @@
+package day5CarryFwdSubArray;
+
+public class Return2DSubArray {
+//	You are given an array A of N integers.
+//	Return a 2D array consisting of all the subarrays of the array
+//	Note : The order of the subarrays in the resulting 2D array does not matter.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
