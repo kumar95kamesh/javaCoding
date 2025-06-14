e611466634457b3fc7094c39706bfe8d9b26ee28
HP <HP@Simple-Buddy>
2025-06-14 19:53:44 +0530

    Project name and moved another projects classes here for centralized system.

---
+++ codingJava/src/day5_oopsSession2/Qusetion.java
@@ -0,0 +1,37 @@
+package day5_oopsSession2;
+
+public class Qusetion {
+//	Build a library system where:
+//		• The library can have Books, magazines and
+//		Journals.
+//		• Some types of items can be loaned and some are
+//		only for reading at library.
+//		• Users (Members: Students, Professors) can
+//		borrow and return different types of Library
+//		items.
+//		• Books have categories (Fiction, Non-fiction,
+//		Academic).
+//		• A loan management system keeps track of issued
+//		books.
+//		Implement features like late fees and
+//		borrowing limits.
+	
+	
+	//Link to the full doc for the approach
+//	https://docs.google.com/document/d/1R5vNGARo5Hm13j57HRRcJ-tRIDKVC2SeaH7CImuSyl8/edit?usp=sharing
+		
+	//Approach
+	/**
+	 * Milestone A:
+
+Represent User structure
+
+• Create the Base User Class
+• Add Constructors to User
+• Create a Subclass Member
+• Create a Subclass Librarian
+• Implement generateUniqueId using Static
+and Final Concepts
+	 */
+
+}
